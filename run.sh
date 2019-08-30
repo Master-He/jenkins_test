76a05a17a4e2e8f631176ec7dd3970de6ace8345
Wenji He <hewj3@lenovo.com>
2019-08-30 10:15:20 +0800

    add redis-volume

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+
+# Create hyperminer redis-db volume
+if [[ "$(docker volume ls | grep redis-volume)" == "" ]]; then
+    docker volume create --name=redis-volume
+fi
+
+
 docker-compose up -d flask
